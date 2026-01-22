e2a45ba9bec8a5185fda995b359fe7a9742fed2f
Baraa105 <baraaqasim105@outlook.com>
2026-01-22 14:31:06 +0100

    fixat stubarna

--- library/include/driver/timer/stub.h
+++ library/include/driver/timer/stub.h
@@ -13,18 +13,17 @@ namespace timer
 /**
  * @brief Timer interface.
  */
-class stub final : public Interface
+class Stub final : public Interface
 {
 public:
      /**
      * @brief constructor.
      */
-         stub() noexcept
+    Stub() noexcept
         : myInitialized{true}
         , myEnabled{false}
         , myTimedOut{false}
-        , myTimeout_ms{0U}
-
+    {}
 
 
     /**
@@ -61,39 +60,66 @@ public:
      * 
      * @return The timeout in milliseconds.
      */
-     uint32_t timeout_ms() const noexcept override { return myTimeout_ms; }
+     uint32_t timeout_ms() const noexcept override 
+     { 
+        // Timeout is irrelevant for the stub, return 0.
+        return 0U; 
+    }
 
     /**
      * @brief Set timeout of the timer.
      * 
      * @param[in] timeout_ms The new timeout in milliseconds.
      */
-     void setTimeout_ms(uint32_t timeout_ms) noexcept override { myTimeout_ms = timeout_ms; }
+     void setTimeout_ms(uint32_t timeout_ms) noexcept override 
+     {
+        // Timeout is irrelevant for the stub, ignore the input.
+        (void) (timeout_ms);
+     }
 
     /**
      * @brief Start the timer.
      */
-     void start() noexcept override { myEnabled = true; myTimedOut = false; } 
+     void start() noexcept override 
+     { 
+        // Starta bara timern om den är initierad, annars gör inget.
+        myEnabled = true; 
+        myTimedOut = false; 
+    } 
 
     /**
      * @brief Stop the timer.
      */
-     void stop() noexcept override { myEnabled = false; myTimedOut = false; }
+     void stop() noexcept override 
+     { 
+        // Stoppa bara timern om den är initierad, annars gör inget.
+        myEnabled = false; 
+        myTimedOut = false; 
+     }
 
     /**
      * @brief Toggle the timer.
      */
-     void toggle() noexcept override { myEnabled = !myEnabled; myTimedOut = false; } 
+     void toggle() noexcept override 
+     {
+        // Toggla bara timern om den är initierad, annars gör inget. 
+        myEnabled = !myEnabled; myTimedOut = false; 
+     } 
 
     /**
      * @brief Restart the timer.
      */
-     void restart() noexcept override { myEnabled = true; myTimedOut = false;} 
-};
+    void restart() noexcept override 
+    {
+        // Starta bara om timern om den är initierad, annars gör inget. 
+        myEnabled = true; 
+        myTimedOut = false;
+    } 
+
     Stub(const Stub&)            = delete; // No copy constructor.
     Stub(Stub&&)                 = delete; // No move constructor.
     Stub& operator=(const Stub&) = delete; // No copy assignment.
-    Stub& operator=(Stub&&)      = delete; // No move assignment.
+    Stub& operator=(Stub&&)      = delete; // No move assignment.x
 
 private:
     bool myInitialized;
@@ -101,8 +127,6 @@ private:
     bool myEnabled;
 
     bool myTimedOut;
-
-    uint32_t myTimeout_ms;
 };
 } // namespace timer
 } // namespace driver
